9262b94a3c47797678e492e694cc6a0e13cb6667
Ishitha10 <ishithabommu1@gmail.com>
2024-09-11 20:31:40 +0100

    Add files via upload

---
+++ main3.py
@@ -0,0 +1,10 @@
+tree1 = 80
+tree2 = 90
+tree3 = 100
+tree4 = 110
+tree5 = 120
+sum = (tree1 + tree2 + tree3 + tree4 + tree5)
+print ("The sum is ",sum)
+average = sum/5
+print (average)
+
---
+++ main4.py
@@ -0,0 +1,7 @@
+x=int(input("Enter a number"))
+h=x//100
+f=(x%100)//50
+g=((x%100)%50)//10
+print("The number of hundreds are", h)
+print("The number of fifties are", f )
+print("The number of tens are", g )
